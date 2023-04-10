096f0732170513e89a66e5fd1664d8cf2741b35a
new user <sanja081107@gmail.com>
2023-04-10 17:53:47 +0300

    make it better

--- invitation/main/views.py
+++ invitation/main/views.py
@@ -11,12 +11,29 @@ def home(request):
     if request.method == 'GET':
         name_man = request.GET.get('name_man')
         name_woman = request.GET.get('name_woman')
-        print(f'имя парня: {name_man}, имя девушки: {name_woman}')
     context = {
         'name_man': name_man,
         'name_woman': name_woman,
     }
     return render(request, 'main/index.html', context)
+
+
+def checking_form(request):
+    if request.method == 'POST':
+        r1 = request.POST['radiobutton']
+        print(r1)
+
+        name_man = request.POST['name_man']
+        name_woman = request.POST['name_woman']
+
+        allergies = request.POST['allergies']
+        night = request.POST['night']
+        tomorrow = request.POST['tomorrow']
+        print(name_man, name_woman, allergies, night, tomorrow)
+
+        return HttpResponse(f"""<a id="form-send" hx-get="/send_mails/?name_man={name_man}&name_woman={name_woman}&allergies={allergies}&night={night}&tomorrow={tomorrow}" 
+                                    hx-trigger="click" hx-target="#form-result" href="#" hidden>Отправить!
+                                </a>""")
 
 
 def send_mails(request):
@@ -39,52 +56,31 @@ def send_mails(request):
         mail = 'alexander_misyuta@mail.ru'
         send_mail(subject, message, 'sanja081107@gmail.com', [mail])
 
-        return HttpResponse("""<div class="alert alert-dismissible fade show u-form-send-message u-form-send-success" style="display: block;" role="alert">
-                                    <strong>Holy guacamole!</strong> Thank you! Your message has been sent.
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>""")
+        return HttpResponse("")
     except:
-        return HttpResponse("""<div class="alert alert-dismissible fade show u-form-send-error u-form-send-message" style="display: block;" role="alert">
-                                    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>""")
+        return HttpResponse("")
 
 
-def checking_form(request):
-    if request.method == 'POST':
-        name_man = request.POST['name_man']
-        name_woman = request.POST['name_woman']
-
-        name = request.POST['name']
-        mail = request.POST['email']
-        address = request.POST['address']
-
-        if name and mail and address:
-            return HttpResponse(f"""<button hx-get="/send_mails/?name_man={name_man}&name_woman={name_woman}" hx-trigger="click" hx-target="#result-main-form" class="u-btn u-button-style">Отправить!</button>""")
-        else:
-            return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
+def not_will_be(request):
+    return HttpResponse("""<div class="u-form-send-error u-form-send-message" style="display: block;">Thank you! Your message has been sent.<a href="#" class="u-form-send-message-close">x</a></div>""")
 
 
-def check_form_name(request):
-    if request.method == 'GET':
-        u = request.GET.get('name')
-    if u == '' or u is None:
-        return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
-
-
-def check_form_email(request):
-    if request.method == 'GET':
-        u = request.GET.get('email')
-    if u == '' or u is None:
-        return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
-
-
-def check_form_address(request):
-    if request.method == 'GET':
-        u = request.GET.get('address')
-    if u == '' or u is None:
-        return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
+# def check_form_name(request):
+#     if request.method == 'GET':
+#         u = request.GET.get('name')
+#     if u == '' or u is None:
+#         return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
+#
+#
+# def check_form_email(request):
+#     if request.method == 'GET':
+#         u = request.GET.get('email')
+#     if u == '' or u is None:
+#         return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
+#
+#
+# def check_form_address(request):
+#     if request.method == 'GET':
+#         u = request.GET.get('address')
+#     if u == '' or u is None:
+#         return HttpResponse("""<button type="submit" class="u-btn u-button-style">Отправить</button>""")
