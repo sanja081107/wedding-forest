bf1a8c65f4a44bdf7c7b478bebbc95eefe81ce12
new user <sanja081107@gmail.com>
2023-05-05 20:59:00 +0300

    make it better

--- invitation/main/admin.py
+++ invitation/main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Wedding, WeddingPhotos
+
+
+class WeddingPhotosInline(admin.TabularInline):
+    model = WeddingPhotos
+    fk_name = 'post'
+    fields = ('photos', 'get_html_photo')
+    readonly_fields = ('get_html_photo',)
+
+    def get_html_photo(self, object):
+        if object.photos:
+            return mark_safe(f"<img src='{object.photos.url}' width=100>")
+    get_html_photo.short_description = 'Миниатюра'
+
+
+class WeddingAdmin(admin.ModelAdmin):
+    model = Wedding
+    inlines = [WeddingPhotosInline, ]
+
+
+admin.site.register(Wedding, WeddingAdmin)
--- invitation/main/models.py
+++ invitation/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+def upload_photos(instance, filename):
+    return 'photos/wedding/{0}/{1}'.format(instance.post.title, filename)
+
+
+class Wedding(models.Model):
+    title = models.CharField(max_length=50, verbose_name='название')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Свадьба'
+        verbose_name_plural = 'Свадьба'
+        ordering = ['title']
+
+
+class WeddingPhotos(models.Model):
+    photos = models.ImageField(upload_to=upload_photos, verbose_name='фото')
+    post = models.ForeignKey(Wedding, related_name='wedding_photos', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Все фото'
