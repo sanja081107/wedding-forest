773800a1f34ff4fc56d706898eea68c02b4e8a7b
new user <sanja081107@gmail.com>
2023-08-04 23:26:46 +0300

    make it better

--- invitation/invitation/settings.py
+++ invitation/invitation/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b-qb_yv--bkigkp@5vrq4x#lr$ku0$-t*(hbtz$^+#^y8i-+rs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
